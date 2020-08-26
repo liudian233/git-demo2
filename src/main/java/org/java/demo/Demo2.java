8aa86a0c563ae97aa3b7029e7d6f2afb6128e33f
liudian233_glb <1242655643@qq.com>
2020-08-26 15:34:26 +0800

    demo2的第二次上传

---
+++ src/main/java/org/java/demo/Demo2.java
@@ -0,0 +1,12 @@
+package org.java.demo;
+
+/**
+ * @author liudian
+ * @version 1.0
+ * @date 2020/8/26 15:33
+ */
+public class Demo2 {
+    public static void main(String[] args) {
+        System.out.println("我是demo2");
+    }
+}
